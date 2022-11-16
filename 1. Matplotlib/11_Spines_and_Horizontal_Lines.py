a0954c51ea1fded40fbc8d3417968983688b19ec
Green Text <regc256@gmail.com>
2022-11-16 02:29:53 -0400

    Added Spines and Candlestick Matplotlib practicing files

--- 1. Matplotlib/11_Spines_and_Horizontal_Lines.py
+++ 1. Matplotlib/11_Spines_and_Horizontal_Lines.py
@@ -8,22 +8,28 @@ def graphData(someData):
     fig = plt.figure(0,constrained_layout=True)
     grids = gridspec.GridSpec(1,1,fig)
     ax1 = fig.add_subplot(grids[0,0])
-    ax_YTicks = []
 
     sourceCode = pd.read_csv(os.path.join(os.path.dirname(__file__),"Files","Example_CSV_Layoffs.csv"))
 
-    ax1.bar(sourceCode["country"],sourceCode["total_laid_off"],)
+    ax1.bar(sourceCode["country"],sourceCode["total_laid_off"])
 
     for labels in ax1.xaxis.get_ticklabels():
         labels.set_rotation(90)
-    ax1.xaxis.label.set_color("cyan")
-    ax1.yaxis.label.set_color("red")
-    
+   
+    #   ax1.xaxis.label.set_color("cyan")
+    #   ax1.yaxis.label.set_color("red")
+   
+    #   ax1.spines["left"].set_color("cyan")
+    ax1.spines["right"].set_visible(False)
+    ax1.spines["top"].set_visible(False)
+    #   ax1.spines["left"].set_linewidth(5)
+
+    ax1.tick_params(axis="x", colors="#444444")
+    #   ax1.axhline(sourceCode["total_laid_off"][1])
     plt.xlabel("Country")
     plt.ylabel("Total Layoffs")
     plt.title(someData)
     plt.legend()
-    plt.subplots_adjust(left=0.09,bottom=0.18,top=1.0,right=0.94,wspace=0.2,hspace=0)
     plt.show()
 
 graphData("Layoffs 2022 per Country")
---
+++ 1. Matplotlib/12_Candlestick_Graphs.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.dates as mdates
+import pandas as pd
+import os
+from mplfinance.original_flavor import candlestick_ohlc
+
+def graphData(someData):
+        fig = plt.figure(0, constrained_layout=True)
+        grids = gridspec.GridSpec(2,1, fig)
+        ax1 = fig.add_subplot(grids[0,0])
+        ax2 = fig.add_subplot(grids[1,0])
+
+        sourceCode = pd.read_csv(os.path.join(os.path.dirname(__file__), "Files", "Example_CSV_ADP_DATA.csv"))
+        startIndex = list(pd.DatetimeIndex(sourceCode["Date"]).year).index(2021)
+        endIndex = list(pd.DatetimeIndex(sourceCode["Date"]).year).index(2022)
+        
+        sourceCode["Date"] = pd.to_datetime(sourceCode["Date"],format="%Y-%m-%d")   #    Convert to datetime format
+
+        ax1.plot(sourceCode["Date"],sourceCode["Close"],label="Close Price")        # Fine
+        
+        ohcl = []
+        while startIndex < endIndex:
+            #   Candlestick_ohlc needs float as date; hence, using date2num to convert to epoch floating format
+            append_me = mdates.date2num(sourceCode["Date"][startIndex]), sourceCode["High"][startIndex], sourceCode["Low"][startIndex], sourceCode["Open"][startIndex], sourceCode["Close"][startIndex], sourceCode["Volume"][startIndex]
+            ohcl.append(append_me)
+            startIndex = startIndex + 1
+        candlestick_ohlc(ax2,ohcl, width=0.4,colorup="g",colordown="r")
+        ax2.xaxis_date()
+        ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))   # Don't want epoch format as dates; hence, converting back.
+
+        plt.xlabel("Date")
+        plt.ylabel("Close Price")
+        plt.title(someData)
+        plt.legend()
+        plt.show()
+graphData("ADP Close Stock Price Data")
